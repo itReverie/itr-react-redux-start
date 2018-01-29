bcc2c51ae60b89e389dc97cbf34748780feb2507
itreverie <brenda.jimenez@itreverie.com>
2018-01-29 12:17:25 -0500

    Fixing example for the table

--- src/docs/examples/ConceptTable/ExampleConceptTable.js
+++ src/docs/examples/ConceptTable/ExampleConceptTable.js
@@ -6,16 +6,11 @@ import ConceptTable from 'components-app/ConceptTable/ConceptTable';
 export default class ExampleConceptTable extends Component{
   render() {
 
-    return (
-            <ConceptTable
-            width= {180}
-            height= {180}
-            headerWidth= {50}
-            rowHeight= {50}
-             data={[
-              { id:1, name: 'Anna', debt: 200000},
-              { id:2, name: 'Hugo', debt: 550000},
-              { id:3, name: 'Louis', debt: 130000}
-            ]}/>
-            )}
+    return <ConceptTable width={800} 
+    height={250} 
+    headerWidth={50} 
+    rowHeight={50} 
+    data={[{ id: 1, name: "Anna", debt: 200000, lastActivityDate: "6th June 2017" },
+     { id: 2, name: "Hugo", debt: 550000, lastActivityDate: "6th June 2017" },
+     { id: 3, name: "Louis", debt: 130000, lastActivityDate: "6th June 2017" }]} />;}
 }
