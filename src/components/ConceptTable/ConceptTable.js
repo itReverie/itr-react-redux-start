54dddb87c173d39233f0e9b05d4a21dab3cdb420
itreverie <brenda.jimenez@itreverie.com>
2018-01-29 12:06:21 -0500

    Adding flexbox,styled-components and styled-system

--- src/components/ConceptTable/ConceptTable.js
+++ src/components/ConceptTable/ConceptTable.js
@@ -2,31 +2,24 @@ import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import { Column, Table } from 'react-virtualized';
 import 'react-virtualized/styles.css'; // only needs to be imported once
+import styled from "styled-components";
+import { responsiveStyle } from "styled-system";
 
+
+const TableStyle = styled.table`
+  text-align: center;
+`;
 
 export default class ConceptTable extends PureComponent {
 
 /** Renders a table based on concepts */
 // TODO: At the moment is hardcoded but it will be dynamic
 render(){
-  return (<Table
-    width={this.props.width}
-    height={this.props.height}
-    headerHeight={this.props.headerWidth}
-    rowHeight={this.props.rowHeight}
-    rowCount={this.props.data.length}
-    rowGetter={({ index }) => this.props.data[index]}>
-    <Column
-      label='Name'
-      dataKey='name'
-      width={100}
-    />
-    <Column
-      width={200}
-      label='Debt'
-      dataKey='debt'
-    />
-  </Table>)
+  return <Table width={this.props.width} height={this.props.height} headerHeight={this.props.headerWidth} rowHeight={this.props.rowHeight} rowCount={this.props.data.length} rowGetter={({ index }) => this.props.data[index]}>
+      <Column width={this.props.width / 3} label="Name" dataKey="name" />
+      <Column width={this.props.width / 3} label="Debt" dataKey="debt" />
+      <Column width={this.props.width / 3} label="Last Activity Date" dataKey="lastActivityDate" />
+    </Table>;
   }
 }
 
---
+++ src/components/TableContainer/TableContainer.js
@@ -0,0 +1,66 @@
+import React, { PureComponent } from "react";
+import PropTypes from 'prop-types';
+import styled from "styled-components";
+import { responsiveStyle } from 'styled-system';
+import ConceptTable from "../ConceptTable/ConceptTable";
+
+
+const flexDirection = responsiveStyle({
+  prop: 'direction',
+  cssProperty: 'flexDirection'
+})
+
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  ${flexDirection};
+`;
+// Create a Title component that'll render an <h1> tag with some styles
+//styled.h1 is a function that when call return a react component that renders and H1
+// You can now call functions with `` and pass those arguments
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+/** Container of a table so it manages the diplay in different sizes */
+export default class TableContainer extends PureComponent {
+                 constructor(props) {
+                   super(props);
+                   this.state = { width: 0, height: 0 };
+                   this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+                 }
+
+                 componentDidMount() {
+                   this.updateWindowDimensions();
+                   window.addEventListener("resize", this.updateWindowDimensions);
+                 }
+
+                 componentWillUnmount() {
+                   window.removeEventListener("resize", this.updateWindowDimensions);
+                 }
+
+                 updateWindowDimensions() {
+                   this.setState({
+                     width: window.innerWidth,
+                     height: window.innerHeight
+                   });
+                 }
+
+                 render() {
+                   return <Flex direction={["column"]}>
+                       <Title>{this.props.title}</Title>
+                       <ConceptTable data={this.props.data} width={this.state.width} height={this.state.height} />
+                       <div>Hello</div>
+                     </Flex>;
+                 }
+               }
+
+ TableContainer.propTypes = { 
+     /** information to render on the table */
+   data: PropTypes.array.isRequired,
+    /** title for the information on the table */
+   title: PropTypes.string.isRequired
+ };
